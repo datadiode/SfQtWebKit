1905f08af4aea614099196524d3989af50a5b51c
datadiode <datadiode@users.noreply.github.com>
2023-01-04 13:41:05 +0100

    Move class Tracer from qttools/src/assistant/assistant/tracer.h to qtbase/src/corelib/global/qlogging.h to make it available to other modules

--- qtbase/src/corelib/global/qlogging.h
+++ qtbase/src/corelib/global/qlogging.h
@@ -187,5 +187,31 @@ Q_CORE_EXPORT void qSetMessagePattern(const QString &messagePattern);
 Q_CORE_EXPORT QString qFormatLogMessage(QtMsgType type, const QMessageLogContext &context,
                                         const QString &buf);
 
+// class Tracer moved here from qttools/src/assistant/assistant/tracer.h
+class Tracer
+{
+public:
+    Tracer(const char *func) : m_func(func)
+    {
+        qDebug("Entering function %s.", m_func);
+    }
+
+    ~Tracer()
+    {
+        qDebug("Leaving function %s.", m_func);
+    }
+
+private:
+    const char * const m_func;
+};
+
 QT_END_NAMESPACE
+
+// #define TRACING_REQUESTED
+#ifdef TRACING_REQUESTED
+#define TRACE_OBJ Tracer traceObj__(Q_FUNC_INFO);
+#else
+#define TRACE_OBJ
+#endif
+
 #endif // QLOGGING_H
--- qttools/src/assistant/assistant/tracer.h
+++ qttools/src/assistant/assistant/tracer.h
@@ -36,32 +36,6 @@
 
 #include <QtCore/QtGlobal>
 
-QT_BEGIN_NAMESPACE
-
-class Tracer
-{
-public:
-    Tracer(const char *func) : m_func(func)
-    {
-        qDebug("Entering function %s.", m_func);
-    }
-
-    ~Tracer()
-    {
-        qDebug("Leaving function %s.", m_func);
-    }
-
-private:
-    const char * const m_func;
-};
-
-QT_END_NAMESPACE
-
-// #define TRACING_REQUESTED
-#ifdef TRACING_REQUESTED
-#define TRACE_OBJ Tracer traceObj__(Q_FUNC_INFO);
-#else
-#define TRACE_OBJ
-#endif
+// class Tracer moved from here to qtbase/src/corelib/global/qlogging.h
 
 #endif // TRACER_H
