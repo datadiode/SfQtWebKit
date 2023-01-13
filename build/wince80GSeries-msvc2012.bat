5ee472f45c0dfd665158bf1e72b1cdbe4841fade
datadiode <datadiode@users.noreply.github.com>
2023-01-13 17:15:06 +0100

    Minor changes to mkspecs and build scripts / Add mkspec and build script for wince80GSeries-msvc2012

--- build/wince80GSeries-msvc2012.bat
+++ build/wince80GSeries-msvc2012.bat
@@ -23,7 +23,14 @@ CALL "C:\Ruby31-x64\bin\setrbvars.cmd"
 
 FOR /F "tokens=1,2,* delims== " %%A IN (%QT_SOURCE%\qtBase\mkspecs\%QMAKESPEC%\qmake.conf) DO IF "%%A" geq "CE_ARCH" IF "%%A" leq "CE_SDK" SET "%%A=%%B" 
 SET CE_SDKDIR=%ProgramFiles(x86)%\Windows CE Tools\SDKs\%CE_SDK%\Sdk
+SET CE_SDKDIR_TORADEX=%ProgramFiles(x86)%\Windows CE Tools\SDKs\Toradex_CE800\Sdk
 SET CE_
+xcopy /S /R /Y "%CE_SDKDIR_TORADEX%\Inc\egl\" "egl\Inc\egl\"
+xcopy /S /R /Y "%CE_SDKDIR_TORADEX%\Inc\GLES\" "egl\Inc\GLES\"
+xcopy /S /R /Y "%CE_SDKDIR_TORADEX%\Inc\GLES2\" "egl\Inc\GLES2\"
+xcopy /T "%CE_SDKDIR%\Lib\" "egl\Lib\"
+FOR %%X IN (libegl libglesv2) DO lib /def:%~dp0egl\%%X.def /machine:%CE_ARCH% /out:egl\Lib\%CE_ARCH%\retail\%%X.lib
+SET OPENGL_ES2=%QT_BUILD%\egl
 
 SETLOCAL
 	SET "INCLUDE=%INCLUDE%;%OPENSSL_INC%;"
@@ -33,7 +40,7 @@ SETLOCAL
 ENDLOCAL
 
 SET "PATH=%CE_SDKDIR%\bin\i386;%CE_SDKDIR%\bin\i386\%CE_ARCH:~0,3%;%PATH%"
-SET "INCLUDE=%CE_SDKDIR%\crt\Include;%CE_SDKDIR%\crt\Include\sys;%CE_SDKDIR%\crt\Include\stl;%CE_SDKDIR%\atlmfc\Include;%CE_SDKDIR%\Inc;%OPENSSL_INC%;"
-SET "LIB=%CE_SDKDIR%\crt\Lib\%CE_ARCH:~0,3%;%CE_SDKDIR%\atlmfc\lib\%CE_ARCH:~0,3%;%CE_SDKDIR%\Lib\%CE_ARCH%\retail;"
+SET "INCLUDE=%CE_SDKDIR%\crt\Include;%CE_SDKDIR%\crt\Include\sys;%CE_SDKDIR%\crt\Include\stl;%CE_SDKDIR%\atlmfc\Include;%CE_SDKDIR%\Inc;%OPENSSL_INC%;%QT_BUILD%\egl\Inc;"
+SET "LIB=%CE_SDKDIR%\crt\Lib\%CE_ARCH:~0,3%;%CE_SDKDIR%\atlmfc\lib\%CE_ARCH:~0,3%;%CE_SDKDIR%\Lib\%CE_ARCH%\retail;%QT_BUILD%\egl\Lib\%CE_ARCH%\retail;"
 
 nmake.exe 2>&1 | perl.exe %QT_SOURCE%\build\tools\tee.pl -t -a "%QT_BUILD%\logfile.log"
