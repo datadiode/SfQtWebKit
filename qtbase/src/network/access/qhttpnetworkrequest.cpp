a3a2e6c27ac83b6a540e072fc7c7064c08d82abe
datadiode <datadiode@users.noreply.github.com>
2023-02-22 06:31:30 +0100

    Revert an early workaround from back when it was unclear why Qt5WebKit was producing an empty Content-Type header on WEC2013 x86

--- qtbase/src/network/access/qhttpnetworkrequest.cpp
+++ qtbase/src/network/access/qhttpnetworkrequest.cpp
@@ -144,11 +144,6 @@ QByteArray QHttpNetworkRequestPrivate::header(const QHttpNetworkRequest &request
     QList<QPair<QByteArray, QByteArray> >::const_iterator it = fields.constBegin();
     QList<QPair<QByteArray, QByteArray> >::const_iterator endIt = fields.constEnd();
     for (; it != endIt; ++it) {
-        //Skip empty headers except for Host
-        if (it->second.isEmpty() && qstricmp(it->first.data(), "host") != 0) {
-            qWarning("skipping HTTP request header with name %s and no value.", it->first.data());
-            continue;
-        }
         ba += it->first;
         ba += ": ";
         ba += it->second;
